c6f3bb9ddb8f7f8d3fd6c0342daba31cf9399095
Esteban Guzmán <57820858+ebanguz@users.noreply.github.com>
2025-08-28 11:56:09 -0400

    manifest

--- __manifest__.py
+++ __manifest__.py
@@ -8,7 +8,7 @@
 Long description of module's purpose
     """,
 
-	'author'     :"My Company of course r  f ff",
+	'author'     :"My Company of course r  f ff nuevo ententdimiento",
 	'website'    :"https://www.yourcompany.com",
 
 	# Categories can be used to filter modules in modules listing
